b487a939193a6596726d3c282ce091c75f0870e7
Clint Bird <clintjb@gmail.com>
2026-04-30 14:02:07 +0200

    Initial Commit

---
+++ motivation/config.js
@@ -0,0 +1,44 @@
+const posters = [
+    {
+        title: "INNUVAYSHUN",
+        author: "President Camacho",
+        quote: "It's got electrolytes, it's what the shareholders crave",
+        regulatory: "Photo: Lazer-Wolf/Dwayne. ELECTROLYTEST™ BRAWNDO: THE THIRST MUTILATOR LLC INC.",
+        image: "images/camacho.jpg" 
+    },
+    {
+        title: "FIGHTING SPIRIT",
+        author: "Yoda, Jedi Master",
+        quote: "Do or do not. There is no try.",
+        regulatory: "Photo: Fighting Taco/mitchell. EMOTIONS © GmbH",
+        image: "images/taco.jpg" 
+    },
+    {
+        title: "STAY HUMBLE",
+        author: "Confucius",
+        quote: "Humility is the solid foundation of all virtues.",
+        regulatory: "Photo: Carragher + Neville/ynwa. EMOTIONS © GmbH",
+        image: "images/carragher.jpg" 
+    },
+    {
+        title: "RESILIENCE",
+        author: "Anonymous",
+        quote: "The strongest steel is forged in the fires of a dumpster",
+        regulatory: "Photo: Great BW meltdown of Q3 2024/dpagent. EMOTIONS GmbH",
+        image: "images/dumpster.jpg" 
+    },
+    {
+        title: "MODESTY",
+        author: "Matshona Dhliwayo",
+        quote: "A flower does not use words to announce its arrival to the world - it just blooms",
+        regulatory: "Photo: Regional Sales Lead / Maserati. UNWARRANTED CONFIDENCE © GmbH",
+        image: "images/maserati.jpg" 
+    },
+    {
+        title: "COMMUNICATION",
+        author: "Anonymous",
+        quote: "A picture isn't always clearer than a thousand words",
+        regulatory: "Corkboard #7 / Tommy's IMAGINAYSHUN © LLC. May contain traces of glitter and glue.",
+        image: "images/dataculture.jpg" 
+    }
+];
